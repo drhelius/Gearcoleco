2e559647eab8018caecc06fbfe2c05565c6319dc
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2024-03-23 11:41:36 +0100

    Basic impl of mem editor

--- platforms/desktop-shared/imgui/memory_editor.cpp
+++ platforms/desktop-shared/imgui/memory_editor.cpp
@@ -36,32 +36,23 @@ void GearMemoryEditor::Draw(uint8_t* mem_data, int mem_size, int base_display_ad
     int columns = 8;
 
     ImGui::BeginChild("##mem", ImVec2(ImGui::GetContentRegionAvail().x, 0), false, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoNav);
-    ImDrawList* draw_list = ImGui::GetWindowDrawList();
 
-    // ImDrawList* draw_list = ImGui::GetWindowDrawList();
-    // ImVec2 line_start = ImGui::GetCursorScreenPos();
-    // ImVec2 line_end = ImVec2(line_start.x + ImGui::GetWindowWidth(), line_start.y);
-
-    float line_height = ImGui::GetTextLineHeight();
     int total_rows = (mem_size + (columns - 1)) / columns;
     int total_columns = columns + 2;
-    static ImGuiTableFlags flags = ImGuiTableFlags_NoKeepColumnsVisible | ImGuiTableFlags_ScrollX | ImGuiTableFlags_RowBg;
-    static ImVec2 cell_padding(4.0f, 0.0f);
+    static ImGuiTableFlags flags = ImGuiTableFlags_NoKeepColumnsVisible | ImGuiTableFlags_RowBg | ImGuiTableFlags_NoPadOuterX | ImGuiTableFlags_NoPadInnerX | ImGuiTableFlags_SizingFixedFit;
+    static ImVec2 cell_padding(0.0f, 0.0f);
 
     ImGui::PushStyleVar(ImGuiStyleVar_CellPadding, cell_padding);
+
     if (ImGui::BeginTable("mem", total_columns, flags))
     {
-        ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, ImGui::CalcTextSize("FFFF: ").x);
-        for (int n = 0; n < columns; n++)
-        {
-            ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed, ImGui::CalcTextSize("FF").x);
-        }
-        ImGui::TableSetupColumn("", ImGuiTableColumnFlags_WidthFixed);
-
         char buf[3];
-
+        ImGui::PushStyleColor(ImGuiCol_FrameBg, 0);
         ImGuiListClipper clipper;
         clipper.Begin(total_rows);
+
+        bool next = false;
+
         while (clipper.Step())
         {
             for (int row = clipper.DisplayStart; row < clipper.DisplayEnd; row++)
@@ -69,9 +60,9 @@ void GearMemoryEditor::Draw(uint8_t* mem_data, int mem_size, int base_display_ad
                 int addr = row * columns;
 
                 ImGui::TableNextRow();
-
                 ImGui::TableNextColumn();
-                ImGui::Text("%04X: ", addr + base_display_addr);
+
+                ImGui::Text("%04X:  ", addr + base_display_addr);
 
                 for (int n = 0; n < columns && addr < mem_size; n++, addr++)
                 {
@@ -79,37 +70,37 @@ void GearMemoryEditor::Draw(uint8_t* mem_data, int mem_size, int base_display_ad
                     
                     sprintf(buf, "%02X", mem_data[addr]);
 
-                    if (m_selected_address == addr)
+                    ImGui::PushID(addr);
+                    ImGuiInputTextFlags flags = ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_NoHorizontalScroll | ImGuiInputTextFlags_CharsUppercase | ImGuiInputTextFlags_AlwaysOverwrite;
+                    ImGui::SetNextItemWidth(20);
+                    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(0, 0));
+
+                    if (next)
                     {
-                        ImGuiInputTextFlags flags = ImGuiInputTextFlags_CharsHexadecimal | ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll | ImGuiInputTextFlags_NoHorizontalScroll;
+                        ImGui::SetKeyboardFocusHere();
+                        next = false;
+                    }
 
-                        bool data_write = false;
-                        if (ImGui::InputText("##mem",buf, sizeof(buf), flags))
-                        {
-                            data_write = true;
-                        }
-
+                    if (ImGui::InputText("##cell", buf, IM_ARRAYSIZE(buf), flags))
+                    {
                         unsigned int data_input_value = 0;
-                        if (data_write && sscanf(buf, "%X", &data_input_value) == 1)
+                        if (sscanf(buf, "%X", &data_input_value) == 1)
                         {
                             mem_data[addr] = (uint8_t)data_input_value;
                         }
+                        next = true;
                     }
-                    else if (ImGui::Selectable(buf, false, ImGuiSelectableFlags_AllowDoubleClick))
-                    {
-                        if (ImGui::IsMouseDoubleClicked(0))
-                        {
-                            m_selected_address = addr;
-                        }
-                    }
+                    ImGui::PopStyleVar();
+                    ImGui::PopID();
                 }
 
                 ImGui::TableNextColumn();
+                addr = row * columns;
                 for (int n = 0; n < columns && addr < mem_size; n++, addr++)
                 {
                     unsigned char c = mem_data[addr];
                     if (n == 0)
-                        ImGui::Text("  ");
+                        ImGui::Text("   ");
 
                     ImGui::SameLine(0.0f, 0.0f);
                     ImGui::Text("%c", (c >= 32 && c < 128) ? c : '.');
@@ -117,55 +108,11 @@ void GearMemoryEditor::Draw(uint8_t* mem_data, int mem_size, int base_display_ad
             }
         }
 
+        ImGui::PopStyleColor();
         ImGui::EndTable();
-        ImGui::PopStyleVar();
     }
-    
 
-    // ImGuiListClipper clipper;
-    // clipper.Begin(line_total_count, line_height);
-
-
-    // ImVec2 window_pos = ImGui::GetWindowPos();
-
-    // draw_list->AddLine(ImVec2(window_pos.x + 50, window_pos.y), ImVec2(window_pos.x + 50, window_pos.y + 9999), ImGui::GetColorU32(ImGuiCol_Border));
-    // draw_list->AddLine(ImVec2(window_pos.x + 300, window_pos.y), ImVec2(window_pos.x + 300, window_pos.y + 9999), ImGui::GetColorU32(ImGuiCol_Border));
-
-    // ImGui::Columns(columns + 2, "mycolumns3", false);  // 3-ways, no border
-    // ImGui::Separator();
-
-    // while (clipper.Step())
-    // {
-    //     for (int line_i = clipper.DisplayStart; line_i < clipper.DisplayEnd; line_i++)
-    //     {
-    //         int addr = line_i * columns;
-    //         ImGui::Text("%04X ", addr + base_display_addr);
-    //         ImGui::NextColumn();
-
-    //         // Draw Hexadecimal
-    //         for (int n = 0; n < columns && addr < mem_size; n++, addr++)
-    //         {
-    //             //ImGui::SameLine();
-    //             //ImGui::SameLine(32 + n * 3 * ImGui::CalcTextSize("FF ").x);
-    //             ImGui::Text("%02X", mem_data[addr]);
-    //             ImGui::NextColumn();
-    //         }
-
-    //         // Draw ASCII
-    //         addr = line_i * columns;
-    //         //ImGui::SameLine(32 + 3 * 16 * ImGui::CalcTextSize("FF ").x + ImGui::CalcTextSize(" ").x);
-    //         //ImGui::SameLine();
-    //         for (int n = 0; n < columns && addr < mem_size; n++, addr++)
-    //         {
-    //             unsigned char c = mem_data[addr];
-    //             //ImGui::SameLine();
-    //             ImGui::Text("%c", (c >= 32 && c < 128) ? c : '.');
-    //         }
-    //         ImGui::NextColumn();
-    //     }
-    // }
-
-    // ImGui::Columns(1);
+    ImGui::PopStyleVar();
 
     ImGui::EndChild();
 }
