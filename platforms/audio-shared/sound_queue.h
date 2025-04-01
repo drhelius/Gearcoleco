b88ca17a79432aba9010c62a7a85050e3e8ca38a
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2025-04-01 20:14:28 +0200

    Incorrect license header

--- platforms/audio-shared/sound_queue.h
+++ platforms/audio-shared/sound_queue.h
@@ -1,6 +1,6 @@
 /*
- * Geargrafx - PC Engine / TurboGrafx Emulator
- * Copyright (C) 2024  Ignacio Sanchez
+ * Gearcoleco - ColecoVision Emulator
+ * Copyright (C) 2021  Ignacio Sanchez
 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
