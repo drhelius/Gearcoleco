b9a4763bceb4cc595e19b187e14436b43b1b5a1c
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-02-08 20:23:28 +0100

    Reset audio after loading media

--- platforms/desktop-shared/emu.cpp
+++ platforms/desktop-shared/emu.cpp
@@ -106,6 +106,7 @@ void emu_destroy(void)
 
 void emu_load_rom(const char* file_path, Cartridge::ForceConfiguration config)
 {
+    emu_audio_reset();
     save_ram();
     gearcoleco->LoadROM(file_path, &config);
     load_ram();
@@ -199,6 +200,7 @@ bool emu_is_bios_loaded(void)
 
 void emu_reset(Cartridge::ForceConfiguration config)
 {
+    emu_audio_reset();
     save_ram();
     gearcoleco->ResetROM(&config);
     load_ram();
