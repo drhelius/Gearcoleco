f9f5417b14e12894653382084a78aacb27ea7f12
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-08-27 00:15:54 +0200

    Minor fix

--- platforms/shared/desktop/gui_menus.cpp
+++ platforms/shared/desktop/gui_menus.cpp
@@ -165,6 +165,7 @@ static void menu_gearcoleco(void)
             ImGui::EndMenu();
         }
 
+        ImGui::Separator();
         ImGui::MenuItem("Enable Softpatching", "", &config_emulator.softpatching);
         if (ImGui::IsItemHovered())
             ImGui::SetTooltip("Automatically applies a matching .ips patch next to the ROM when loading.");
